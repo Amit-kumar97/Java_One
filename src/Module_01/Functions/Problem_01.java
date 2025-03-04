cfaf01feacdfdd54408ae0cd67c3d14b5037e0b2
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2025-03-04 23:39:52 +0530

    Functions Problems

---
+++ src/Module_01/Functions/Problem_01.java
@@ -0,0 +1,28 @@
+package Module_01.Functions;
+
+/*
+    Function Problem 1
+    Write a function such that a function fun will be called and "I am another function" will be printed.
+
+    Input Format
+    none
+
+    Output Format
+    "I am another function" is to be printed without quotations as below
+
+    Example 1
+    Input
+
+    Output
+    I am another function
+ */
+public class Problem_01 {
+    //Make a function here and call it in main()
+    public static void fun() {
+        // write code here
+        System.out.println("I am another function");
+    }
+    public static void main(String[] args) {
+        fun();
+    }
+}
